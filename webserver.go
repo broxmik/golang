265db1d11a9ad26e6a05ee798b0cb60e1d0869b1
broxmik <roxanabajan@gmail.com>
2014-10-11 15:20:58 +0200

    fix server port

--- webserver.go
+++ webserver.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"github.com/russross/blackfriday"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":" + port, nil)
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
